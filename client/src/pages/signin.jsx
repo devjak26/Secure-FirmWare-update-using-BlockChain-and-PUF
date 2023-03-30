e0d7706686999fd5489983d33e8e5d2be5d3284c
AashishA11 <76701146+AashishA11@users.noreply.github.com>
2023-03-30 20:54:08 +0530

    particle.js added
    particle.js added

--- client/src/pages/signin.jsx
+++ client/src/pages/signin.jsx
@@ -12,9 +12,28 @@ import {
   MDBCheckbox
 }
 from 'mdb-react-ui-kit';
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
 
 function SignInPage() {
+
+  const particlesInit = useCallback(async engine => {
+		console.log(engine);
+		// you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+		// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+		// starting from v2 you can add only the features you need reducing the bundle size
+		await loadFull(engine);
+	}, []);
+	
+	const particlesLoaded = useCallback(async container => {
+		await console.log(container);
+	}, []);
+
+
   return (
+     <div> 
     <MDBContainer fluid className="login">
 
       <MDBRow className='d-flex justify-content-center align-items-center h-100'>
@@ -24,7 +43,7 @@ function SignInPage() {
             <MDBCardBody className='p-5 w-100 d-flex flex-column'>
 
               <h2 className="fw-bold mb-2 text-center">Sign in</h2>
-              <p className="text-white-50 mb-3">Please enter your login and password!</p>
+              <p className="fw-bold mb-2 text-center">Please enter your login and password!</p>
 
               <MDBInput wrapperClass='mb-4 w-100' label='Email address' id='formControlLg' type='email' size="lg"/>
               <MDBInput wrapperClass='mb-4 w-100' label='Password' id='formControlLg' type='password' size="lg"/>
@@ -44,6 +63,88 @@ function SignInPage() {
       </MDBRow>
 
     </MDBContainer>
+    <div>
+    <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                background: {
+                    // color: {
+                    //     value: "#0d47a1",
+                    // },
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 6,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+
+    </div>
+    </div>
+     
   );
 }
 
